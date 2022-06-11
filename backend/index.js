f87f1e97b1b89f3a69fac55ca2bb4781d4c67d12
Chandhana <chandhanareddy1997@gmail.com>
2022-06-12 02:53:14 +0530

    basic server

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  return res.send("hello buddy!!");
+});
+
+app.listen(port, () => console.log(`Im listening on port, ${port}`));
