5c3eb8383cba5b7ec26e89cf31abdfd363a39f82
Chandhana <chandhanareddy1997@gmail.com>
2022-06-20 08:11:28 +0530

    user schema update and category model

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,44 +2,47 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 const { v4: uuidv4 } = require("uuid");
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    maxLength: 32,
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 32,
+    },
+    lastname: {
+      type: String,
+      trim: true,
+      maxLength: 32,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    userinfo: {
+      type: String,
+      trim: true,
+    },
+    // TODO: password
+    encry_password: {
+      type: String,
+      required: true,
+    },
+    // for hashing passwords
+    salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
+    purchases: {
+      type: Array,
+      default: [],
+    },
   },
-  lastname: {
-    type: String,
-    trim: true,
-    maxLength: 32,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  userinfo: {
-    type: String,
-    trim: true,
-  },
-  // TODO: password
-  encry_password: {
-    type: String,
-    required: true,
-  },
-  // for hashing passwords
-  salt: String,
-  role: {
-    type: Number,
-    default: 0,
-  },
-  purchases: {
-    type: Array,
-    default: [],
-  },
-});
+  { timestamps: true }
+);
 // virtual fields
 userSchema
   .virtual("password")
@@ -53,6 +56,9 @@ userSchema
     return this._password;
   });
 userSchema.method = {
+  authenticate: function (plainPassword) {
+    return this.securePassword(plainPassword) === this.encry_password;
+  },
   // user is gonna pass password
   securePassword: function (plainPassword) {
     if (!password) return "";
