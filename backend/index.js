eb7029a24ef798df763703719c9b45de14fb8619
Chandhana <chandhanareddy1997@gmail.com>
2022-06-25 06:04:39 +0530

    database connections

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("mongodb connected successfully!!"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   return res.send("hello buddy!!");
