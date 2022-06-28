24b4fd8df98e4bc6ae5ad577f77670a77880d7db
Chandhana <chandhanareddy1997@gmail.com>
2022-06-28 17:19:06 +0530

    middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cookieparser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,6 +12,11 @@ mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => console.log("mongodb connected successfully!!"))
   .catch((err) => console.log(err));
+
+// middlewares
+app.use(bodyparser.json());
+app.use(cookieparser());
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.send("hello buddy!!");
