577cd146e84b15815c29d0ad4ff77c79fe3bb6f7
Chandhana <chandhanareddy1997@gmail.com>
2022-07-10 20:05:50 +0530

    signin

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,3 +1,6 @@
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 // bringing models
 const User = require("../models/user");
 exports.signout = (req, res) => {
@@ -5,6 +8,10 @@ exports.signout = (req, res) => {
 };
 
 exports.signup = (req, res) => {
+  const errors = validationResult(req, res);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ error: errors.array()[0].msg });
+  }
   const user = new User(req.body);
   // to save into db
   user.save((err, user) => {
@@ -18,5 +25,33 @@ exports.signup = (req, res) => {
       email: user.email,
       id: user._id,
     });
+  });
+};
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  // validation check
+  const errors = validationResult(req, res);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ error: errors.array()[0].msg });
+  }
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res
+        .status(400)
+        .json({ error: "This email doesn't exist in our database" });
+    }
+    if (!user.authenticate(password)) {
+      return res
+        .status(401)
+        .json({ error: "Email and Password do not match!" });
+    }
+    // creating a token
+    const authToken = jwt.sign({ _id: user._id }, process.env.SECRET);
+    // keeping token in cookie
+    res.cookie("token", authToken, { expire: new Date() + 9999 });
+    // sending response to frontend
+    const { _id, name, email, role } = user;
+    return res.json({ authToken, user: { _id, name, email, role } });
   });
 };
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 // controllers
-const { signout, signup } = require("../controllers/auth");
+const { signout, signup, signin } = require("../controllers/auth");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  body("name").isLength({ min: 3 }).withMessage("invalid name"),
+  body("email").isEmail().withMessage("Please enter a valid email address"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Please enter a valid password"),
+  signup
+);
+
+router.post(
+  "/signin",
+  body("email").isEmail().withMessage("Please enter a valid email address"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Please field is required")
+    .isLength({ min: 1 }),
+  signin
+);
 router.get("/signout", signout);
 
 module.exports = router;
